3518a4dc324ef1c0b1b1c0ad8ac3e0f22b70c6d3
Dirtyking103 <dirtyking103@gmail.com>
2024-09-05 11:09:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,7 +24,7 @@ MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/Moviesearchgrou
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002233069580 -1002203224103').split()]
 DATABASE_URI = environ.get('DATABASE_URI', "")
 
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster1")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Dirtyking")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002244630898'))
 QR_CODE = environ.get('QR_CODE', 'https://graph.org/file/ccb9db43e62a2e524928e.jpg')
