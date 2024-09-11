8e7f5dc4b979c55589b74a4d155738be2a121d6f
Dirtyking103 <dirtyking103@gmail.com>
2024-09-11 18:05:28 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -9,8 +9,6 @@ async def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
     return web_app
-async def example_handler(request):
-    return web.Response(text="Hello, world!", content_type='text/plain')
     
 async def check_expired_premium(client):
     while 1:
