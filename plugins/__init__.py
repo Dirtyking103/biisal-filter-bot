ed3a5df4d8de7852bc7a78942ea26d6ef7566f30
Dirtyking103 <dirtyking103@gmail.com>
2024-09-11 17:36:47 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -8,7 +8,6 @@ from info import LOG_CHANNEL
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
-    return web_app
 async def example_handler(request):
     return web.Response(text="Hello, world!", content_type='text/plain')
     
